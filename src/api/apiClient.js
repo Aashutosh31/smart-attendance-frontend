82a3fdb2f28d351c0bb9681e4ccf958d8e8699e4
paradigmshiftershcc <hexacorecoders@gmail.com>
2025-09-25 17:19:55 +0530

    A

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 // src/api/apiClient.js
 import { useAuthStore } from "../store/AuthStore"; // Import the auth store
 
-const API_URL = "https://django-bice-seven.vercel.app/api";
+const API_URL = "https://django-bice-seven.vercel.app/api/register-college/";
 
 const apiClient = {
   registerCollege: async (formData) => {
